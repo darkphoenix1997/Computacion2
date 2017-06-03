5ebc05a18037a1ee7bd7231d3ae71ec9845b664a
Luis Manuel García Valdivia <darkfenix.lmgv@gmail.com>
2017-06-03 13:19:46 -0500

    Update Proyecto111.py

--- Clasificador de Imagenes/Proyecto111.py
+++ Clasificador de Imagenes/Proyecto111.py
@@ -1,4 +1,3 @@
-
 # coding: utf-8
 
 # In[ ]:
@@ -10,28 +9,35 @@ import os
 
 
 class Imagen():
-  def __init__(self,Name,Rout,Label,Weigth):
-    self.Name=Name
-    self.Rout=Rout
-    self.Label=Label
-    self.Weigth=Weigth
+	def __init__(self, Name, Rout, Label, Weigth):
+		self.Name = Name
+		self.Rout = Rout
+		self.Label = []
+		self.Weigth = Weigth
+	def __repr__(self):
+		return (self.Name)
 
-mypat="C:\Users\Luis Manuel\Desktop\Proyecto"
 
-class Labels():  
-    if os.path.isdir(mypat):
-	  for i in listdir(mypat):
-	    if os.path.isdir(mypat+"/"+i):
-	      pass
-	    elif os.path.isfile(mypat+"/"+i):
-	      if i.endswith(".jpg") or (".jpeg") or (".gif") or (".png"):
-	        img=Image.open(mypat+"/"+i)
-	        img.show()
-	        etiquetas={}
-	        a=raw_input("Ponga su etiqueta:")	      
-	        if a in etiquetas:
-	          etiquetas[a].append(Imagen(i,mypat,a,os.stat(mypat+"/"+i)[6]))
-	        else:
-	          etiquetas[a]=[Imagen(i,mypat,a,os.stat(mypat+"/"+i)[6])]
-	        img.close()
+mypat = "C:\Users\Luis Manuel\Desktop\Proyecto"
 
+
+class Labels():
+	if os.path.isdir(mypat):
+		for i in listdir(mypat):
+			if os.path.isdir(mypat + "/" + i):
+				pass
+			elif os.path.isfile(mypat + "/" + i):
+				if i.endswith(".jpg") or (".jpeg") or (".gif") or (".png"):
+					img = Image.open(mypat + "/" + i)
+					img.show()
+					etiquetas = {}
+					a = raw_input("Etiqueta:")
+					if a in etiquetas:
+						etiquetas[a].append(Imagen(i, mypat, a, os.stat(mypat + "/" + i)[6]))
+					else:
+						etiquetas[a] = [Imagen(i, mypat, a, os.stat(mypat + "/" + i)[6])]
+					img.close()
+
+					print etiquetas
+					b=raw_input("Categoria:") #Convertir a metodo.
+					print 'Existe', len(etiquetas[b]),'imagenes en esta categoria:',etiquetas[a]
