a09ad4bec8fa5f202ec69b49b5ef189025082075
Luis Manuel García Valdivia <darkfenix.lmgv@gmail.com>
2017-06-04 16:44:27 -0500

    Update Proyecto111.py

--- Clasificador de Imagenes/Proyecto111.py
+++ Clasificador de Imagenes/Proyecto111.py
@@ -9,6 +9,7 @@ from PIL import Image
 import os
 from Tkinter import *
 from PIL import ImageTk, Image
+import Tkinter as tk
 
 class Imagen():
 	def __init__(self, Name, Rout, Label, Weigth):
@@ -30,28 +31,55 @@ class Labels():
 				pass
 			elif os.path.isfile(mypat + "/" + i):
 				if i.endswith(".jpg") or (".jpeg") or (".gif") or (".png"):
-					root = Tk()   #INTERFAZ GRAFICA.
-					img = ImageTk.PhotoImage(Image.open(mypat + "/" + i))
-					panel = Label(root, image=img)
-					panel.pack(side="bottom", fill="both", expand="yes")
-					root.mainloop()
+					#INTERFAZ GRAFICA.(en proceso).
 
-					etiquetas = {}  #DICCIONARIO.
-					a = raw_input("Etiqueta:")
-					if a in etiquetas:
-						etiquetas[a].append(Imagen(i, mypat, a, os.stat(mypat + "/" + i)[6]))
-					else:
-						etiquetas[a] = [Imagen(i, mypat, a, os.stat(mypat + "/" + i)[6])]
+					def hacer_click():
+						valor2 = entrada_txt.get()
+						etiqueta.config(text=valor2)
 
-					print etiquetas
-					b=raw_input("Categoria:")  #CONVERTIR A METODO.
-					if b in etiquetas:
-						print 'Existe', len(etiquetas[b]),'imagenes en esta categoria:',etiquetas[a]
-					else:
-						print "No es posible encontrar esa categoria"
+					app = Tk()
+					app.title("Clasificador de imagenes")
+					app.geometry('700x500')
+					app.configure(bg='dark turquoise')
+					vp = Frame(app)
+					vp.grid(column=0, row=0, padx=(50, 50), pady=(10, 10,))
 
-					c=raw_input("Imagen a buscar:")  #CONVERTIR A METODO.
-					if c in etiquetas:
-						print etiquetas[a]
-					else:
+					try:
+						img = ImageTk.PhotoImage(Image.open('11.jpeg'))    #prueba.
+						panel = Label(app, image=img)
+					except:
+						'Comprueba tu  archivo.'
+
+					valor = ""
+					entrada_txt = Entry(vp, width=10, textvariable=valor)
+					entrada_txt.grid(column=3, row=1)
+
+					vp.columnconfigure(0, weight=1)
+					vp.rowconfigure(0, weight=1)
+					etiquetaz = Label(vp, text="Etiqueta:",fg='white')
+					etiquetaz.grid(column=1, row=1)
+					boton = Button(vp, text="Listo.",bg='dark turquoise',fg='white', command=hacer_click)
+					boton.grid(column=4, row=1)
+
+					app.mainloop()
+
+	#				etiquetas = {}  #DICCIONARIO.
+	#				a = raw_input("Etiqueta:")
+	#				if a in etiquetas:
+	#					etiquetas[a].append(Imagen(i, mypat, a, os.stat(mypat + "/" + i)[6]))
+	#				else:
+	#					etiquetas[a] = [Imagen(i, mypat, a, os.stat(mypat + "/" + i)[6])]
+#
+#					print etiquetas
+#					b=raw_input("Categoria a buscar:")  #CONVERTIR A METODO.
+#					if b in etiquetas:
+#						print 'Existe', len(etiquetas[b]),'imagenes en esta categoria:',etiquetas[a]
+#					else:
+#						print "No es posible encontrar esa categoria."
+#
+#					c=raw_input("Imagen a buscar:")  #CONVERTIR A METODO.
+#					if c in etiquetas:
+#						print etiquetas[a]
+#					else:
+#						print "Verifique la etiqueta del elemento a acceder."
 						print "Verifique la etiqueta del elemento a acceder"
